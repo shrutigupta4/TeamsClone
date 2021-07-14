b7fb15e24f90f4cf0152a84fb04596836278789f
Shruti <04shrutigupta@gmail.com>
2021-07-14 13:36:28 +0530

    server issue

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ var cookieParser = require('cookie-parser')
 
 
 //Google auth
-const {OAuth2Client} = require('google-auth-library');
+const { OAuth2Client } = require('google-auth-library');
 const CLIENT_ID = '1099106946396-at69kokb2ni7fh0ikjadbg7knsf7ko69.apps.googleusercontent.com'
 const client = new OAuth2Client(CLIENT_ID);
 
@@ -21,6 +21,8 @@ const client = new OAuth2Client(CLIENT_ID);
 const roomsMap = {}
 
 const Message = require('./models/messages')
+
+const User = require('./models/users')
 const mongoose = require('mongoose');
 
 const dotenv = require('dotenv')
@@ -35,7 +37,7 @@ mongoose.connect(process.env.CONNECTIONSTRING, {
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
+db.once('open', function () {
   // we're connected!
 });
 
@@ -47,7 +49,7 @@ app.set('view engine', 'ejs')
 
 app.use(express.static('public'))
 app.use(express.json()); //to send json from frontend to backend
-app.use(cookieParser()); 
+app.use(cookieParser());
 
 
 //Routes
@@ -55,17 +57,17 @@ app.get('/', (req, res) => {
   res.render('login');
 })
 
-app.get('/login', (req, res)=>{
+app.get('/login', (req, res) => {
   res.render('login')
 })
 
-app.post('/login', (req, res)=>{
+app.post('/login', (req, res) => {
   let token = req.body.token; //google token sent on login from frontend
 
   async function verify() {
     const ticket = await client.verifyIdToken({
-        idToken: token,
-        audience: CLIENT_ID,  //the CLIENT_ID of the app that accesses the backend
+      idToken: token,
+      audience: CLIENT_ID,  //the CLIENT_ID of the app that accesses the backend
     });
     const payload = ticket.getPayload();
     const userid = payload['sub'];
@@ -73,24 +75,24 @@ app.post('/login', (req, res)=>{
     // const domain = payload['hd'];
   }
   verify()
-  .then(()=>{
-    res.cookie('session-token', token);
-    res.send('success');
-  }).catch(console.error);
+    .then(() => {
+      res.cookie('session-token', token);
+      res.send('success');
+    }).catch(console.error);
 })
 
-app.get('/dashboard', checkAuthenticated, (req, res)=>{
+app.get('/dashboard', checkAuthenticated, (req, res) => {
   let user = req.user;
-    roomId = user.email;
-  res.render('dashboard', {roomId, user});
+  roomId = user.email;
+  res.render('dashboard', { roomId, user });
 })
 
 
 
-app.get('/logout', (req, res)=>{
+app.get('/logout', (req, res) => {
   res.clearCookie('session-token');
   res.redirect('/login');
-})  
+})
 
 app.get('/room', (req, res) => {
   res.redirect(`/${uuidV4()}`)
@@ -98,56 +100,95 @@ app.get('/room', (req, res) => {
 
 app.get('/:room', checkAuthenticated, (req, res) => {
   let user = req.user;
-  res.render('room', { roomId: req.params.room, user})
+  res.render('room', { roomId: req.params.room, user })
 })
 
 
-function checkAuthenticated(req, res, next){
+function checkAuthenticated(req, res, next) {
 
   let token = req.cookies['session-token'];
 
   let user = {};
   async function verify() {
-      const ticket = await client.verifyIdToken({
-          idToken: token,
-          audience: CLIENT_ID,  // Specify the CLIENT_ID of the app that accesses the backend
-      });
-      const payload = ticket.getPayload();
-      user.name = payload.name;
-      user.email = payload.email;
-      user.picture = payload.picture;
-    }
-    verify()
-    .then(()=>{
-        req.user = user;
-        next();
+    const ticket = await client.verifyIdToken({
+      idToken: token,
+      audience: CLIENT_ID,  // Specify the CLIENT_ID of the app that accesses the backend
+    });
+    const payload = ticket.getPayload();
+    user.name = payload.name;
+    user.email = payload.email;
+    user.picture = payload.picture;
+  }
+  verify()
+    .then(() => {
+      req.user = user;
+      next();
     })
-    .catch(err=>{
-        res.redirect('/login') //if authentication failed
+    .catch(err => {
+      res.redirect('/login') //if authentication failed
     })
 
 }
 
 io.on('connection', socket => {
-  
+
   socket.on('join-room', (roomId, userId, userName) => {
     socket.join(roomId)
+    async function addNewRoom() {
+      let userdb = await db.collection("users").find({ useremail: userId });
+      userdb =  await userdb.toArray()
+      if (!userdb.length) {
+        console.log("here")
+          userdb = new User({
+          useremail: userId,
+          rooms: roomId
+        })
+        userdb.save()
+      }
+      
+      await db.collection("users").updateOne(
+        {'useremail': userId},
+        {'$addToSet': { 'rooms': roomId }
+      }
+      
+        
+      )
+
+    }addNewRoom().catch(console.error)
+
+
+    socket.on('message', message => {
+      //send message to the same room
+      const messagedb = new Message({
+        room: roomId,
+        user: userName,
+        message_body: message,
+        time: new Date().toLocaleTimeString([], { hour: '2-digit', minute: "2-digit" })
+      });
+      messagedb.save(); //updating on the database
+
+      io.to(roomId).emit('createMessage', { messagedb });
+
+    });
+
 
     //to send a user's rooms to dashboard
-    async function sendRooms(){
-      const resultss = await db.collection("users").find({useremail: userId}).toArray();
-          io.to(socket.id).emit('showRoomIds', resultss)
-    }sendRooms().catch(console.error)
+    async function sendRooms() {
+      var resultss = await db.collection("users").find({ useremail: userId });
+      resultss =  await resultss.toArray();
+      io.to(socket.id).emit('showRoomIds', resultss)
+    } sendRooms().catch(console.error)
 
     //load messages of the given room
     async function getMessages() {
-      const results = await db.collection("messages").find({room: roomId}).toArray();
+      var results = await db.collection("messages").find({ room: roomId });
+      results =  await results.toArray();
       io.to(socket.id).emit('createMessage', results)
     }
     getMessages().catch(console.error);
 
 
-    if(roomsMap[roomId]) {
+    if (roomsMap[roomId]) {
       roomsMap[roomId][userId] = userName;
 
 
@@ -157,11 +198,11 @@ io.on('connection', socket => {
       roomsMap[roomId] = peersMap;
     }
 
-    
+
 
     socket.broadcast.to(roomId).emit('user-connected', userId)
     socket.broadcast.to(roomId).emit('addToParticipants', userName) //to add to participants list
-    
+
 
     socket.on('disconnect', () => {
       socket.broadcast.to(roomId).emit('user-disconnected', userId)
@@ -173,16 +214,16 @@ io.on('connection', socket => {
       const messagedb = new Message({
         room: roomId,
         user: userName,
-       message_body: message,
-       time: new Date().toLocaleTimeString([], { hour: '2-digit', minute: "2-digit" })
-     });
-     messagedb.save(); //updating on the database
+        message_body: message,
+        time: new Date().toLocaleTimeString([], { hour: '2-digit', minute: "2-digit" })
+      });
+      messagedb.save(); //updating on the database
 
-      io.to(roomId).emit('createMessage', {messagedb});
-      
-    }); 
+      io.to(roomId).emit('createMessage', { messagedb });
+
+    });
   })
-  socket.on('count', roomId =>{
+  socket.on('count', roomId => {
     io.to(socket.id).emit('peerss', roomsMap[roomId]) //names of peers sent to clients
   })
 });
@@ -190,4 +231,3 @@ io.on('connection', socket => {
 
 const port = process.env.PORT || 3000;
 server.listen(port, () => console.info('Server is running on port', `${port}`))
-console
